b8f08fdf16a20fc00c814c6ee54911c3ed0fb0af
Krish Patel <krishbipinpatel@gmail.com>
2025-02-23 03:24:44 +0530

    Edited Realtime news thing

--- nexus-frontend/src/pages/Dashboard.tsx
+++ nexus-frontend/src/pages/Dashboard.tsx
@@ -13,14 +13,14 @@ export default function Dashboard() {
         <h1 className="text-3xl font-bold mb-8 text-white">
           Nexus of Truth Dashboard
         </h1>
-        <Tabs defaultValue="realtime-news" className="w-full">
-          <TabsList className="grid w-full grid-cols-4 gap-4 mb-8 rounded-lg h-auto bg-slate-800">
-            <TabsTrigger
+        <Tabs defaultValue="nlp-model" className="w-full">
+          <TabsList className="grid w-full grid-cols-3 gap-4 mb-8 rounded-lg h-auto bg-slate-800">
+            {/* <TabsTrigger
               value="realtime-news"
               className="p-3 bg-gray-800 text-white hover:bg-gray-700 data-[state=active]:bg-blue-600"
             >
               Realtime News Checking
-            </TabsTrigger>
+            </TabsTrigger> */}
             <TabsTrigger
               value="nlp-model"
               className="p-3 bg-gray-800 text-white hover:bg-gray-700 data-[state=active]:bg-blue-600"
@@ -42,9 +42,9 @@ export default function Dashboard() {
           </TabsList>
 
           <div className="mt-4">
-            <TabsContent value="realtime-news">
+            {/* <TabsContent value="realtime-news">
               <RealtimeNews />
-            </TabsContent>
+            </TabsContent> */}
             <TabsContent value="deepfake-detection">
               <div className="w-full h-[800px]">
                 <iframe
